0ede2cc612cabe942077e87993701e907f8b8361
Greg <gregory.martin.data@gmail.com>
2023-12-28 16:26:58 +0100

    Updated logger_config

--- logger_config.py
+++ logger_config.py
@@ -7,13 +7,13 @@ def setup_logging():
     # File handler configuration
     file_handler = logging.FileHandler("app.log")
     file_handler.setLevel(logging.WARNING)
-    file_format = logging.Formatter("%(asctime)s - %(message)s", "%Y-%m-%d %H:%M:%S")
+    file_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S")
     file_handler.setFormatter(file_format)
 
     # Basic configuration
     logging.basicConfig(
         level="INFO",
-        format="%(asctime)s - %(message)s",
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         handlers=[RichHandler(), file_handler],
     )
